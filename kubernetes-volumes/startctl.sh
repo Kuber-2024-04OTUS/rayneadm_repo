b6a637f134ded5e5405e86923ab965a63d4a8622
rayne <rayne@mail.ru>
2024-06-09 20:55:52 +0300

    fix all tasks without *

--- kubernetes-volumes/startctl.sh
+++ kubernetes-volumes/startctl.sh
@@ -3,5 +3,7 @@ kubectl label nodes minikube homework=true
 kubectl apply -f ./namespace.yaml
 kubectl apply -f ./service.yaml
 kubectl apply -f ./configmap.yaml
+kubectl apply -f ./cm.yaml
+kubectl apply -f ./pvc.yaml
 kubectl apply -f ./deployment.yaml
 kubectl apply -f ./ingress.yaml
